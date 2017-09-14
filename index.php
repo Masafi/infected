67289ef77e74e55385f1f3517fb39982157a9b1e
Iskandarov Lev <iskandarov11@inbox.ru>
2017-09-14 22:30:06 +0300

    Uploaded files

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+        <meta charset="UTF-8" />
+		<title>
+			Infected
+		</title>
+		<script type="text/javascript" src="phaser-ce/build/phaser.min.js"></script>
+	</head>
+	<body>
+	<script type="text/javascript">
+		
+    </script>
+	</body>
+</html>
