daf0ab97f95e34d6909d206df5566a1be4ee13cf
Iskandarov Lev <iskandarov11@inbox.ru>
2017-09-15 13:14:35 +0300

    Developing

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!--<html>
+<html>
 	<head>
         <meta charset="UTF-8" />
 		<title>
@@ -9,18 +9,5 @@
 	</head>
 	<body>
 		<script type="text/javascript" src="src/game.js"></script>
-	</body>
-</html>-->
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Chat</title>
-		<link href="css/style.css" rel="stylesheet">
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
-		<script src="src/client.js"></script>
-	</head>
-	<body>
-		<div id="log"></div><br>
-		<input type="text" id="input" autofocus><input type="submit" id="send" value="Send">
 	</body>
 </html>
